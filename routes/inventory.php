f4f6cc0895cb2c501dc5417b90e04af86bb93843
mubeenmobi <72138002+mubeenmobi@users.noreply.github.com>
2022-03-03 14:43:15 +0500

    route file changed

--- routes/inventory.php
+++ routes/inventory.php
@@ -1,1 +1,23 @@
 <?php
+
+use Illuminate\Support\Facades\Route;
+use Logixs\Controllers\Modules\Inventory\Controllers\InventoryDashboardController;
+use Logixs\Controllers\Modules\Inventory\Controllers\Category\CategoryIndexController;
+use Logixs\Controllers\Modules\Inventory\Controllers\Category\CategoryStoreController;
+use Logixs\Controllers\Modules\Inventory\Controllers\Category\CategoryUpdateController;
+use Logixs\Controllers\Modules\Inventory\Controllers\SubjectArea\SubjectAreaIndexController;
+use Logixs\Controllers\Modules\Inventory\Controllers\SubjectArea\SubjectAreaStoreController;
+
+Route::prefix('admin')->middleware(['auth'])->group(function () {
+    Route::get('inventory/dashboard', InventoryDashboardController::class)->name('inventory.dashboard');
+
+    //category
+    Route::get('inventory/category', CategoryIndexController::class)->name('inventory.category-index');
+    Route::post('inventory/category/store', CategoryStoreController::class)->name('inventory.category-store');
+    Route::post('inventory/category/{id}/update', CategoryUpdateController::class)->name('inventory.category-update');
+
+    //subject area
+    Route::get('inventory/subject-area', SubjectAreaIndexController::class)->name('inventory.subject-index');
+    Route::post('inventory/subject-area/store', SubjectAreaStoreController::class)->name('inventory.subject-store');
+    Route::post('inventory/subject-area/{id}/update', SubjectAreaStoreController::class)->name('inventory.subject-update');
+});
--- routes/web.php
+++ routes/web.php
@@ -3,12 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Logixs\Controllers\Auth\LoginController;
 use Logixs\Controllers\AdminDashboardController;
-use Logixs\Controllers\Modules\Inventory\Controllers\InventoryDashboardController;
-use Logixs\Controllers\Modules\Inventory\Controllers\Category\CategoryIndexController;
-use Logixs\Controllers\Modules\Inventory\Controllers\Category\CategoryStoreController;
-use Logixs\Controllers\Modules\Inventory\Controllers\Category\CategoryUpdateController;
-use Logixs\Controllers\Modules\Inventory\Controllers\SubjectArea\SubjectAreaIndexController;
-use Logixs\Controllers\Modules\Inventory\Controllers\SubjectArea\SubjectAreaStoreController;
 
 Route::get('/', [LoginController::class, 'showLoginForm'])->name('login.show');
 Route::post('login', [LoginController::class, 'login'])->name('login');
@@ -19,15 +13,6 @@ Route::middleware(['auth'])->group(function () {
 
 Route::prefix('admin')->middleware(['auth'])->group(function () {
     Route::get('dashboard', AdminDashboardController::class)->name('admin.dashboard');
-
-    Route::get('inventory/dashboard', InventoryDashboardController::class)->name('inventory.dashboard');
-    //category
-    Route::get('inventory/category', CategoryIndexController::class)->name('inventory.category-index');
-    Route::post('inventory/category/store', CategoryStoreController::class)->name('inventory.category-store');
-    Route::post('inventory/category/{id}/update', CategoryUpdateController::class)->name('inventory.category-update');
-
-    //subject area
-    Route::get('inventory/subject-area', SubjectAreaIndexController::class)->name('inventory.subject-index');
-    Route::post('inventory/subject-area/store', SubjectAreaStoreController::class)->name('inventory.subject-store');
-    Route::post('inventory/subject-area/{id}/update', SubjectAreaStoreController::class)->name('inventory.subject-update');
 });
+
+require __DIR__.'/inventory.php';
