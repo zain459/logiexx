e20fdcaa109f4446c82e4363adf5d6c361e1b040
mubeenmobi <72138002+mubeenmobi@users.noreply.github.com>
2022-03-03 14:49:12 +0500

    dashboard updated

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -23,6 +23,18 @@
                     </a>
                 </div>
 
+                <div class="col shadow-sm border bg-white rounded">
+                    <a class="d-block p-3 text-body text-decoration-none" href="{{route('inventory.dashboard')}}">
+                        <div class="p-2 rounded d-inline-block mb-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="text-primary" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.253v13m0-13C10.832 5.477 9.246 5 7.5 5S4.168 5.477 3 6.253v13C4.168 18.477 5.754 18 7.5 18s3.332.477 4.5 1.253m0-13C13.168 5.477 14.754 5 16.5 5c1.747 0 3.332.477 4.5 1.253v13C19.832 18.477 18.247 18 16.5 18c-1.746 0-3.332.477-4.5 1.253" />
+                            </svg>
+                        </div>
+                        <h4>Courses</h4>
+                        <p class="text-muted">Manage courses, courses category, subject areas.</p>
+                    </a>
+                </div>
+
             </div>
     </div>
 @endsection
