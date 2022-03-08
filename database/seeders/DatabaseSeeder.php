23081c5964b291bad31d9fb6d0eee57e40b9dbf1
mubeenmobi <72138002+mubeenmobi@users.noreply.github.com>
2022-03-08 12:55:09 +0500

    seeder created

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Logixs\Modules\User\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $this->createUser();
+
+    }
+
+    public function createUser(): void
+    {
+        $user = new User();
+        $user->name = 'Admin';
+        $user->email = 'admin@admin.com';
+        $user->setPassword('secret');
+        $user->save();
     }
 }
