bc6b882fb9ba803ad2a9682465e9be374affd9a3
mubeenmobi <72138002+mubeenmobi@users.noreply.github.com>
2022-03-10 15:39:58 +0500

    testimonials link added

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -59,6 +59,18 @@
                     </a>
                 </div>
 
+                <div class="col shadow-sm border bg-white rounded">
+                    <a class="d-block p-3 text-body text-decoration-none" href="{{ route('testimonial-index') }}">
+                        <div class="p-2 rounded d-inline-block mb-3">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="text-success" viewBox="0 0 20 20" fill="currentColor">
+                                <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+                            </svg>
+                        </div>
+                        <h4>Testimonials</h4>
+                        <p class="text-muted">Testimonials.</p>
+                    </a>
+                </div>
+
             </div>
     </div>
 @endsection
--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -40,8 +40,8 @@
 
                 <li class="sidebar-item">
                     <a class="sidebar-link" href="{{ route('testimonial-index') }}">
-                        <svg xmlns="http://www.w3.org/2000/svg"  width="24" height="24" viewBox="0 0 20 20" fill="currentColor">
-                            <path d="M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z" />
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
                         </svg>
 
                         <span class="align-middle">Testimonials</span>
